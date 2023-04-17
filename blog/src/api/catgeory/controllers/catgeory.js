aff0d56314b5c43226069992da4c7daf95520c28
Robert Romulus <46854878+digitalmediaflow@users.noreply.github.com>
2023-04-17 12:17:32 -0400

    two dummy blog posts

---
+++ blog/src/api/catgeory/controllers/catgeory.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * catgeory controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::catgeory.catgeory');
---
+++ blog/src/api/catgeory/services/catgeory.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * catgeory service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::catgeory.catgeory');
