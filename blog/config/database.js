13d10896faa679d0813be0fe743b716cabaf35ef
Robert Romulus <46854878+digitalmediaflow@users.noreply.github.com>
2023-04-17 21:16:31 -0400

    updates on strapi dependencies

--- blog/config/database.js
+++ blog/config/database.js
@@ -1,16 +1,11 @@
-module.exports = ({ env }) => ({
+const path = require("path");
+
+module.exports = () => ({
   connection: {
-    client: 'postgres',
+    client: "sqlite",
     connection: {
-      host: env('PGHOST', 'containers-us-west-190.railway.app'),
-      port: env.int('PGPORT', 5919),
-      database: env('PGDATABASE', 'railway'),
-      user: env('PGUSER', 'postgres'),
-      password: env('PGPASSWORD', 'uYImv3uIY0x7zsKtagfj'),
-      ssl: env.bool(true),
+      filename: path.join(__dirname, "..", ".tmp/data.db"),
     },
+    useNullAsDefault: true,
   },
 });
-
-
-
